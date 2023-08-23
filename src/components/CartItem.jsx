a8e3256a8f205c923c9e12cee027bad8d5dda01f
Francisco Berti da Cruz <fbc991@live.com>
2023-08-22 22:50:15 -0300

    .

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -1,4 +1,26 @@
-const CartItem = () => {
-  return <div>CartItem</div>;
+import { ChevronDown, ChevronUp } from '../icons';
+
+const CartItem = ({ amount, id, img, price, title }) => {
+  return (
+    <article className="cart-item">
+      <img src={img} alt={title} />
+      <div>
+        <h4>{title}</h4>
+        <h4 className="item-price">{price}</h4>
+        <button type="button" className="remove-btn">
+          remove
+        </button>
+      </div>
+      <div>
+        <button type="button" className="amount-btn">
+          <ChevronUp />
+        </button>
+        <p className="amount">{amount}</p>
+        <button type="button" className="amount-btn">
+          <ChevronDown />
+        </button>
+      </div>
+    </article>
+  );
 };
 export default CartItem;
